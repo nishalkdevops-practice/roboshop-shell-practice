8359033d450405958184280c2670cf5abb01e6f1
nishal <nishalk9399@gmail.com>
2024-08-20 16:28:16 +0530

    pushing ec2 automation script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-0b4f379183e5706b9
+SECURITY_GROUP_ID=sg-034b8c610c8a714d3
+
 
 for i in "${NAMES[@]}"
 do 
-    echo "NAME: $i"
+    if [ [ $i == "mongodb" || $i == "mysql" ] ]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi 
+
+    echo "creating $i instance
+    aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]"
+
 done
 
 
