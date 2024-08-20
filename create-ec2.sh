93d66d0af0b90d6832fef2935272d56d9ebd606a
nishal <nishalk9399@gmail.com>
2024-08-20 16:33:53 +0530

    pushing ec2 automation script

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,14 +8,14 @@ SECURITY_GROUP_ID=sg-034b8c610c8a714d3
 
 for i in "${NAMES[@]}"
 do 
-    if [ [ $i == "mongodb" || $i == "mysql" ] ]
+    if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi 
 
-    echo "creating $i instance
+    echo "creating $i instance"
     aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]"
 
 done
